7c456761b235717af44bbd12dd991fc717f2040f
chingliu <chingliu12@gmail.com>
2018-08-03 10:52:55 +0300

    nav was designed but there was a suggestion to increase the link size

--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
-//  the date code
+
 var hDate = document.querySelector('#hDate');
 
+//  the date code
 var d = new Date();
-hDate.textContent = d.getDate() + "/" + (d.getMonth() + 1) + "/" + d.getFullYear() + " Tips";
+hDate.textContent = d.getDate() + "/" + (d.getMonth() + 1) + "/" + d.getFullYear() + " free Tips";
 hDate.style.textAlign = "center";
